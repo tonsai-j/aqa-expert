b4b52256e8e3ea83e9503682d106a38a7a191ec5
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-09-08 17:23:10 +0700

    convert date

--- backend/app/controllers/profile.controller.js
+++ backend/app/controllers/profile.controller.js
@@ -1,10 +1,27 @@
 exports.update = function (req, res) {
-    var data = {};
-    data[req.body.field] = req.body.data;
+    var data = req.body.data;
+    var obj = {}
+    if (Array.isArray(data)) {
+        for (var index in data) {
+            data[index] = convertDate(data[index]);
+        }
+    } else {
+        data = convertDate(data)
+    }
+    obj[req.body.field] = data;
     var profile = r.table('profile').get(req.body.id);
-    r.branch(profile.eq(null), null, profile.update(data))
+    r.branch(profile.eq(null), null, profile.update(obj))
         .run()
         .then(function (data) {
             res.json(data)
         })
+    function convertDate(data) {
+        for (var key in data) {
+            if (key.indexOf('date_') > -1 || key.indexOf('_date') > -1) {
+                data[key] = r.ISO8601(data[key]).inTimezone('+07')
+            }
+        }
+        return data;
+    }
+
 }
