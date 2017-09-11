865425eaba08bf3904947dcbec2d097d23eb52b4
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-09-11 11:48:46 +0700

    group faculty

--- backend/app/controllers/type/academy.controller.js
+++ backend/app/controllers/type/academy.controller.js
@@ -1,7 +1,7 @@
 exports.list = function (req, res) {
     var query = r.table('type_academy');
-    if (typeof req.query.work_group_id !== 'undefined' && req.query.work_group_id != '')
-        query = query.getAll(req.query.work_group_id, { index: 'work_group_id' })
+    if (typeof req.query.group_work_id !== 'undefined' && req.query.group_work_id != '')
+        query = query.getAll(req.query.group_work_id, { index: 'group_work_id' })
 
     query
         .orderBy('id')
