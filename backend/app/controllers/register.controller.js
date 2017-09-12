229c8dbeb53acece871637a013076f88a6f8361f
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-09-12 15:26:45 +0700

    insert basic convert date

--- backend/app/controllers/register.controller.js
+++ backend/app/controllers/register.controller.js
@@ -5,30 +5,38 @@ exports.post = function (req, res) {
         r.table('profile').get(
             r.table('profile').insert(
                 r.expr(req.body)
-                    .merge({
-                        type_assessor: r.table('type_assessor').get(req.body.type_assessor_id),
-                        // basic: {},
-                        address: {
-                            address1: {},
-                            address2: {},
-                            address_selected: ''
-                        },
-                        education: [],
-                        specialist: [],
-                        working: [],
-                        skills: {
-                            computer: {},
-                            edu: {},
-                            lang: {},
-                            type: {}
-                        },
-                        assessment: [],
-                        researches: [],
-                        training: [],
-                        bank: {},
-                        zone: [],
-                        references: [],
-                        documents: []
+                    .merge(function (m) {
+                        return {
+                            type_assessor: r.table('type_assessor').get(req.body.type_assessor_id),
+                            basic: m('basic').merge(function (m2) {
+                                return r.branch(m2.hasFields('birth_date'), {
+                                    birth_date: r.ISO8601(m2('birth_date')).inTimezone('+07'),
+                                    expire_date: r.ISO8601(m2('expire_date')).inTimezone('+07'),
+                                    issue_date: r.ISO8601(m2('issue_date')).inTimezone('+07')
+                                }, {})
+                            }),
+                            address: {
+                                address1: {},
+                                address2: {},
+                                address_selected: ''
+                            },
+                            education: [],
+                            specialist: [],
+                            working: [],
+                            skills: {
+                                computer: {},
+                                edu: {},
+                                lang: {},
+                                type: {}
+                            },
+                            assessment: [],
+                            researches: [],
+                            training: [],
+                            bank: {},
+                            zone: [],
+                            references: [],
+                            documents: []
+                        }
                     })
             )('generated_keys')(0)
         ),
