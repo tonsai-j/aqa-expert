3880c0f5b59150739e6c6224dc03a439c252d5f3
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-09-13 18:36:49 +0700

    change db

--- backend/config/config.js
+++ backend/config/config.js
@@ -13,7 +13,7 @@ var config = {
         { host: 'rdb.codeunbug.com', port: 28015, user: 'admin', password: 'next@2017' }
         // {host: '192.168.10.15', port: 28015}
       ],
-      db: 'expert'
+      db: 'aqa_expert'
     },
 
     oauth: {
