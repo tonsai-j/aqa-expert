c9c85928ed21072daa1ad2e674b1bdfa601140d9
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-09-14 11:12:00 +0700

    issue -> approve

--- backend/app/routes/profile.routes.js
+++ backend/app/routes/profile.routes.js
@@ -2,4 +2,5 @@ module.exports = function (app) {
     var control = require('../controllers/profile.controller')
     app.put('/', control.update);
     app.get('/meeting', control.meeting);
+    
 }
