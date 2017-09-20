d25d82f65998f57fc02295d2f93ca8f410c12d4d
lissara.k <lissara.k@nexts-corp.com>
2017-09-20 18:52:54 +0700

    update cart

--- backend/app/controllers/report/assessor.controller.js
+++ backend/app/controllers/report/assessor.controller.js
@@ -1,11 +1,12 @@
 var path = require('path');
+
 exports.cardId = function (req, res) {
     // console.log(req.query.id)
     r.table('profile').get(req.query.id)
         .merge(function (mm) {
             return {
                 image: r.db('aqa_expert').table('image_file').getAll([req.query.id, 'profile'], { index: "user_idPic_type" })
-                // .getAll(['2387c215-b769-4147-819c-291ab0c98152', 'profile'], { index: "user_idPic_type" })
+                    // .getAll(['2387c215-b769-4147-819c-291ab0c98152', 'profile'], { index: "user_idPic_type" })
                     .coerceTo('array')
             }
         })
@@ -22,14 +23,17 @@ exports.cardId = function (req, res) {
                 file_path: m('image')('file_path')(0)
             }
         })
-        .pluck('taxno', 'name', 'type_assessor_name', 'address', 'file_name','file_path')
+        .pluck('taxno', 'name', 'type_assessor_name', 'address', 'file_name', 'file_path')
 
 
         .run()
         .then(function (data) {
+            var parameters = {
+                IMAGE_DIR: path.resolve('../', data.file_path, data.file_name),
+            };
+            console.log(parameters)
             // res.json(data)
-            console.log(path.resolve('../', data.file_path, data.file_name))
-            res.ireport("reports/expert_card.jasper", req.query.export || "pdf", data)
+            res.ireport("reports/expert_card.jasper", req.query.export || "pdf", data, parameters)
         })
     // res.json('card id')
 }
