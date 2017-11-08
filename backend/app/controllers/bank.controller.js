c791398d7452d360645cae4280d5af0f4f980a9d
Tonsai jompun <tonsai.j@nexts-corp.com>
2017-11-08 16:28:59 +0700

    update page bank

--- backend/app/controllers/bank.controller.js
+++ backend/app/controllers/bank.controller.js
@@ -1,6 +1,14 @@
 exports.list = function (req, res) {
-    r.table('bank')
-        .orderBy('bank_name_th')
+    r.db('aqa_cds').table('bank')
+        .filter({ ACTIVE: true })
+        .merge((row) => {
+            return {
+                label: row('BANKDESC'),
+                value: row('BANKCD'),
+            }
+        })
+        .orderBy('PRIORITY')
+        .pluck('label', 'value')
         .run()
         .then(function (data) {
             res.json(data)
